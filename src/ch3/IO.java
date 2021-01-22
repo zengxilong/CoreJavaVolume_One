67509efe571399b470e0780f5201e7f7cd17e927
zengxilong <1637545108@qq.com>
2021-01-22 18:57:42 +0800

    fix ch3/IO.java

--- src/ch3/IO.java
+++ src/ch3/IO.java
@@ -12,18 +12,18 @@ import java.nio.file.Paths;
 import java.util.*;
 public class IO {
     public static void main(String[] args) throws IOException {
-//        //1
-//        //构造Scanner对象与System.in关联
-//        System.out.println("*************(1)*************");
-//        Scanner in = new Scanner(System.in);
-//
-//        System.out.println("What is you name?");
-//        String name = in.nextLine();
-//
-//        System.out.println("How old are you?");
-//        int age = in.nextInt();
-//
-//        System.out.println("Hello "+name+". Next year you'll be "+(age+1));
+        //1
+        //构造Scanner对象与System.in关联
+        System.out.println("*************(1)*************");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is you name?");
+        String name = in.nextLine();
+
+        System.out.println("How old are you?");
+        int age = in.nextInt();
+
+        System.out.println("Hello "+name+". Next year you'll be "+(age+1));
 
         //2
         //文件输出使用PrintWriter
@@ -35,9 +35,9 @@ public class IO {
 
         //3
         //Scanner读取文件
-        Scanner in = new Scanner(Paths.get("myfile.txt"),"UTF-8");
-        System.out.println(in.next());
-        System.out.println(in.next());
-        in.close();
+        Scanner Fin = new Scanner(Paths.get("myfile.txt"),"UTF-8");
+        System.out.println(Fin.next());
+        System.out.println(Fin.next());
+        Fin.close();
     }
 }
